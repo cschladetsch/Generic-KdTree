9d9761abfc9c3df3b30680697e2b20bca8e1b496
Christian <cschladetsch@powercor.com.au>
2023-03-05 18:23:11 +1100

    Remove catch-based tests as it seems to now suck.

--- tests/TestKdTree.cpp
+++ tests/TestKdTree.cpp
@@ -1,31 +1,6 @@
-﻿#include "pch.h"
-#include "TestKdTree.h"
-#if __has_include("TestKdTree.g.cpp")
-#include "TestKdTree.g.cpp"
-#endif
+#define TWOBLUECUBES_CATCH_HPP_INCLUDED
+#include "catch.hpp"
+#include <KdTree/KdTree.hpp>
 
-using namespace winrt;
-using namespace Windows::UI::Xaml;
 
-namespace winrt::BingKDTree::implementation
-{
-    TestKdTree::TestKdTree()
-    {
-        InitializeComponent();
-    }
 
-    int32_t TestKdTree::MyProperty()
-    {
-        throw hresult_not_implemented();
-    }
-
-    void TestKdTree::MyProperty(int32_t /* value */)
-    {
-        throw hresult_not_implemented();
-    }
-
-    void TestKdTree::ClickHandler(IInspectable const&, RoutedEventArgs const&)
-    {
-        Button().Content(box_value(L"Clicked"));
-    }
-}
